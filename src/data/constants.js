993ed39855ae4d776a9683327712cab89e245327
sharmilat0017 <sharmila@tabletoptech.in>
2025-07-26 17:04:55 +0530

    my first commit

---
+++ src/data/constants.js
@@ -0,0 +1,255 @@
+export const Bio = {
+  name: "Durga Prasad Koppisetti",
+  roles: [
+    "Senior Software Engineer",
+    "Full Stack Developer",
+    "Front End Developer",
+    "Angular Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Prasad0526/prasad-portfolio",
+  resume:
+  "https://drive.google.com/file/d/1Fx2W90poxAU2XaTkLyxkHjwK19GWvCpy/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/durga-prasad-koppisetti-586465228/",
+  // insta: "https://www.instagram.com/seshu_______/",
+  // facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "Angular",
+        image:
+          "https://img.icons8.com/color/48/angularjs.png",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Database",
+    skills: [
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://w7.pngwing.com/pngs/28/601/png-transparent-sql-logo-illustration-microsoft-azure-sql-database-microsoft-sql-server-database-blue-text-logo-thumbnail.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png",
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Laravel",
+        image:
+          "https://img.icons8.com/stickers/100/laravel.png",
+      },
+      {
+        name: "PHP",
+        image:
+          "https://img.icons8.com/stickers/100/laravel.pnghttps://img.icons8.com/color/48/php.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Spring",
+        image:
+          "https://img.icons8.com/?size=512&id=90519&format=png",
+      },
+      {
+        name: "SpringBoot",
+        image:
+          "https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/postman.svg",
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://tabletoptech.in/wp-content/uploads/2022/12/table-1024x323.png",
+    role: "Senior Software Engineer",
+    company: "TableTop Technologies",
+    date: "Feb 2024 - Present",
+    desc: "Led the development of complex web applications using Angular and TypeScript, ensuring high performance and scalability. Designed and implemented RESTful APIs to integrate front-end applications with backend services. Mentored junior developers and conducted code reviews to maintain code quality and best practices. Utilized RxJS for reactive programming and state management in Angular applications. Worked on AWS for deploying and managing cloud-based applications. Implemented CI/CD pipelines using Jenkins and GitHub Actions for automated builds and deployments.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Angular",
+      "PHP",
+      "Laravel",
+      "MySQL",
+      "TypeScript","RxJS","jQuery"
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "https://www.aapthitech.com/assets/img/logo.png",
+    role: "Senior Software Engineer",
+    company: "Aapthi Technologies",
+    date: "JAN 2020 - JAN 2024",
+    desc: "Performed module analysis and database design for web applications. Collaborated with clients and team members to gather requirements and implement solutions. Developed user interfaces using HTML5, CSS3, Bootstrap, JavaScript, and Angular. Enhanced application performance through Angular component-based development, ensuring smooth transitions to future Angular frameworks. Created custom directives, decorators, and services to interface with RESTful APIs. Implemented Angular Router for seamless navigation between application views. Utilized AJAX and JSON for server requests and data exchange between browsers and servers. Managed version control using Git and utilized NPM scripts for build and test environments.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Angular",
+      "PHP",
+      "Laravel",
+      "MySQL",
+      "TypeScript","RxJS","jQuery"
+    ],
+    doc: "",
+  },
+  {
+    id: 0,
+    img: "",
+    role: "Software Engineer",
+    company: "EthreyaS",
+    date: "SEP 2017 - DEC 2019",
+    desc: "Performed module analysis and database design for web applications. Collaborated with clients and team members to gather requirements and implement solutions. Developed user interfaces using HTML5, CSS3, Bootstrap, JavaScript, and Angular. Enhanced application performance through Angular component-based development, ensuring smooth transitions to future Angular frameworks. Created custom directives, decorators, and services to interface with RESTful APIs. Implemented Angular Router for seamless navigation between application views. Utilized AJAX and JSON for server requests and data exchange between browsers and servers. Managed version control using Git and utilized NPM scripts for build and test environments.",
+    skills: [
+      "HTML",
+      "CSS",
+      "Angular",
+      "PHP",
+      "Laravel",
+      "MySQL",
+      "TypeScript","RxJS","jQuery"
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "",
+    school: "Chaitanya Engineering College,Madhavapatnam,Kakinada",
+    date: "",
+    grade: "",
+    desc: "I had completed my Bachelor's degree in information technology at Chaitanya Engineering College, Kakinada.",
+    degree: "Bachelor of Technology - BTech, information technology",
+  },
+  {
+    id: 1,
+    img: "",
+    school: "Ideal Junior College , Kakinada",
+    date: "",
+    grade: "",
+    desc: "I completed my Inter Mediate at Ideal Junior College , Kakinada, where I studied MPC.",
+    degree: "Inter Mediate, MPC",
+  },
+  {
+    id: 2,
+    img: "",
+    school: "Guild Of Service High School, Kakinada",
+    date: "",
+    grade: "",
+    desc: "I completed my class 10 education at Guild Of Service High School, Kakinada, where I SSC.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Insurance Rater",
+    date: "OCT 2020 to Presenet",
+    description:
+    "Developed a responsive web application using Laravel 10 for backend and Angular 15 for frontend to support core insurance workflows. Conducted module analysis, designed and normalized MySQL database schemas, and implemented RESTful APIs. Collaborated closely with cross-functional teams to gather requirements, troubleshoot issues, and deploy productionready features. Integrated complex business logic for quoting, policy generation, user management, and role-based access control. Ensured responsiveness and cross-browser compatibility while optimizing performance using Angular best practices. Followed Agile methodologies and used Git for version control",
+    image:
+      "https://www.insurancerater.com/assets/images/logo.png?v=223344",
+    tags: ["Laravel 10", "PHP 7", "Angular 15", "MySQL"],
+    category: "Full Stack Development",
+    github: "",
+    webapp: "",
+  },
+  // {
+  //   id: 3,
+  //   title: "Bus Ticket Reservation System",
+  //   date: "Jan 2023",
+  //   description:
+  //   "A Console based Bus Ticket Booking System Where users can interact with console and Book Tickets for the available routes it can also generate a payslip based on the booking cost.",
+  //   image:
+  //     "https://user-images.githubusercontent.com/110565344/221412365-74a20f45-5d8d-4b5d-8549-1f9c6c14405a.png",
+  //   tags: ["Core Java", "Collections","Eclipse"],
+  //   category: "Core Java",
+  //   github: "https://github.com/Seshu916/Travel-Desk",
+  //   webapp: "https://github.com/Seshu916/Travel-Desk",
+  // },
+  
+];
+
+export const TimeLineData = [
+  { year: 2022, text: "Started my journey" },
+  { year: 2023, text: "Done a course on Core Java" },
+  { year: 2023, text: "Learned HTML,CSS,Java Script" },
+  { year: 2023, text: "Done a course on Full Stack Java Development" },
+  { year: 2024, text: "Worked on Real time Project" },
+];
